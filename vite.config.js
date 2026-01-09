eadb13d19eb38a584d217cd072ea9dc3048483c2
Torkel0825 <dtk000825@gmail.com>
2026-01-09 11:15:04 +0100

    fixed depolyment error

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/Restaurant-thorin-app.git/",
+  base: "/Restaurant-thorin-app/",
 });
